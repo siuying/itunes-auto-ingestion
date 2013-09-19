3b999bae01b43be68d8d5b127779abfe80ec3665
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2013-09-20 00:34:08 +1000

    Replace deprecated autoload with requires
    see https://www.ruby-forum.com/topic/3036681

--- lib/itunes_ingestion.rb
+++ lib/itunes_ingestion.rb
@@ -1,6 +1,6 @@
+require 'itunes_ingestion/fetcher'
+require 'itunes_ingestion/sales_report_parser'
+
 module ITunesIngestion
   class ITunesConnectError < RuntimeError; end
-  
-  autoload :Fetcher, 'itunes_ingestion/fetcher'
-  autoload :SalesReportParser, 'itunes_ingestion/sales_report_parser'
 end
